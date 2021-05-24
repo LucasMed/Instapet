e818550284abc4b7b6e16217c5cc3f6acf493260
lucasmed <medinalucas.j@gmail.com>
2021-05-24 01:07:46 -0300

    Se agrega el componente PhotoCard.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyles } from './GlobalStyles'
+import { PhotoCard } from './components/PhotoCard/' 
 
 export const App = () => (
   <div>
     <GlobalStyles />
     <ListOfCategories />
+    <PhotoCard />
   </div>
 )
