2b0eadc6f5d876119e96a8a7bb1540d3f0a0bca6
lucasmed <medinalucas.j@gmail.com>
2021-05-25 20:36:31 -0300

    Se controla la carga de la imagen.

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,21 +1,38 @@
-import React from 'react'
-import { ImgWrapper, Img, Button } from './styles'
+import React, { useRef, useEffect,useState, Fragment } from 'react'
+import { ImgWrapper, Img, Button, Article } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
 
 const DEFAULT_IMAGE = "https://i.imgur.com/dJa0Hpl.jpg"
 
-export const PhotoCard = ({ id, likes = 0, src=DEFAULT_IMAGE }) => {
-  return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src}/>
-        </ImgWrapper>
-      </a>
+export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const element = useRef(null);
+  const [show, setShow] = useState(false)
 
-      <Button>
-        <MdFavoriteBorder size='32px' /> {likes} likes!
+  useEffect(() => {
+    const observer = new window.IntersectionObserver(function (entries) {
+      const { isIntersecting } = entries[0];
+      if (isIntersecting) {
+        setShow(true)
+        observer.disconnect()
+      }
+    })
+    observer.observe(element.current)
+  }, [element])
+
+  return (
+    <Article ref={element}>
+      { show && <Fragment>
+        <a href={`/detail/${id}`}>
+          <ImgWrapper>
+            <Img src={src} />
+          </ImgWrapper>
+        </a>
+
+        <Button>
+          <MdFavoriteBorder size='32px' /> {likes} likes!
       </Button>
-    </article>
+      </Fragment>}
+
+    </Article>
   )
 }
