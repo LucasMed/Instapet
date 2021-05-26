f99c2cf3e988906d8179f7eaecec947aeb991e88
lucasmed <medinalucas.j@gmail.com>
2021-05-25 22:34:39 -0300

    Se crea el hook useNearScreen.

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -2,32 +2,14 @@ import React, { useRef, useEffect, useState, Fragment } from 'react'
 import { ImgWrapper, Img, Button, Article } from './styles'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { useNearScreen } from '../../hooks/useNearScreen'
 
 const DEFAULT_IMAGE = "https://i.imgur.com/dJa0Hpl.jpg"
 
 export const PhotoCard = ({ key, id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const element = useRef(null);
-  const [show, setShow] = useState(false)
+  const [show, element] = useNearScreen();
   const itemKey = `like-${id}`
   const [liked, setLiked] = useLocalStorage(itemKey, false)
-
-
-  useEffect(() => {
-    Promise.resolve(
-      typeof window.IntersectionObserver !== undefined
-        ? window.IntersectionObserver
-        : import('intersection-observer')
-    ).then(() => {
-      const observer = new window.IntersectionObserver(function (entries) {
-        const { isIntersecting } = entries[0];
-        if (isIntersecting) {
-          setShow(true)
-          observer.disconnect()
-        }
-      })
-      observer.observe(element.current)
-    })
-  }, [element])
 
   const Icon = liked ? MdFavorite : MdFavoriteBorder;
 
