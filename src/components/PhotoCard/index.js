29acc264dd6710c2fbacc25e9440f45ad5543253
lucasmed <medinalucas.j@gmail.com>
2021-05-25 21:23:11 -0300

    Se verifica el intersection observer dependiendo el soporte.

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect,useState, Fragment } from 'react'
+import React, { useRef, useEffect, useState, Fragment } from 'react'
 import { ImgWrapper, Img, Button, Article } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
 
@@ -9,14 +9,20 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, setShow] = useState(false)
 
   useEffect(() => {
-    const observer = new window.IntersectionObserver(function (entries) {
-      const { isIntersecting } = entries[0];
-      if (isIntersecting) {
-        setShow(true)
-        observer.disconnect()
-      }
+    Promise.resolve(
+      typeof window.IntersectionObserver !== undefined
+        ? window.IntersectionObserver
+        : import('intersection-observer')
+    ).then(() => {
+      const observer = new window.IntersectionObserver(function (entries) {
+        const { isIntersecting } = entries[0];
+        if (isIntersecting) {
+          setShow(true)
+          observer.disconnect()
+        }
+      })
+      observer.observe(element.current)
     })
-    observer.observe(element.current)
   }, [element])
 
   return (
